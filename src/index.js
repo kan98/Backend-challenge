c8d7d5d88e95e2548fa0958215f3c6330c741a0b
kan <kan.srimanchantha@gmail.com>
2020-06-16 13:46:40 +0700

    fixed deprecation warning

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var db = require("./db_setup").myurl;
 require("./routes");
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Database is connected");
   })
